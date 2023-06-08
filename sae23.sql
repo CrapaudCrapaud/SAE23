70ab7a117be86b766d7f15290a3732c02f18f05f
Elie <133014379+CrapaudCrapaud@users.noreply.github.com>
2023-06-08 12:10:21 +0200

    Modification du fichier SQL de la base de données
    Ajout des données pour des raisons de test en local

--- sae23.sql
+++ sae23.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1deb5ubuntu1
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Hôte : localhost:3306
--- Généré le : ven. 02 juin 2023 à 18:00
--- Version du serveur : 8.0.33-0ubuntu0.22.04.2
--- Version de PHP : 8.1.2-1ubuntu2.11
+-- Host: localhost
+-- Generation Time: Jun 08, 2023 at 12:14 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,105 +18,145 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `sae23`
+-- Database: `sae23`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `Administration`
+-- Table structure for table `Administration`
 --
 
 CREATE TABLE `Administration` (
-  `login` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `login` varchar(25) NOT NULL,
   `password` varchar(64) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `Administration`
+--
+
+INSERT INTO `Administration` (`login`, `password`) VALUES
+('admin', 'e4abae53cc1cebe5fe89ea93882c699a5e71ab0bbf42a83b7d833975b61c4a41');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `Batiment`
+-- Table structure for table `Batiment`
 --
 
 CREATE TABLE `Batiment` (
-  `id_bat` tinyint NOT NULL,
-  `nom_bat` varchar(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `login_gest` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `id_bat` tinyint(4) NOT NULL,
+  `nom_bat` varchar(1) NOT NULL,
+  `login_gest` varchar(25) NOT NULL,
   `password_gest` varchar(64) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `Batiment`
+--
+
+INSERT INTO `Batiment` (`id_bat`, `nom_bat`, `login_gest`, `password_gest`) VALUES
+(1, 'B', 'gest1', '2908476d0ff2b0f6195677d324173b69ed53e397fd1d7fef206758c5516ca6a7'),
+(2, 'E', 'gest2', 'b57b66b18e6689677f2c3e8b9da2f1ae4e9ffbf013c8d6da3389b0fc49bb0797'),
+(4, 'C', 'azer', 'd4b3dfbf113cc8b2f6fd71bcb24b761d04b47c04a59b22a2a7db91b275542892');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `Capteur`
+-- Table structure for table `Capteur`
 --
 
 CREATE TABLE `Capteur` (
-  `id_capt` tinyint NOT NULL,
-  `id_bat` tinyint NOT NULL,
-  `nom_capt` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `type_capt` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `id_capt` tinyint(4) NOT NULL,
+  `id_bat` tinyint(4) NOT NULL,
+  `nom_capt` varchar(25) NOT NULL,
+  `type_capt` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `Capteur`
+--
+
+INSERT INTO `Capteur` (`id_capt`, `id_bat`, `nom_capt`, `type_capt`) VALUES
+(1, 2, 'E208-luminosité', 'luminosité'),
+(2, 2, 'E208-température', 'température'),
+(3, 1, 'B106-luminosité', 'luminosité'),
+(4, 1, 'B106-température', 'température');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `Mesure`
+-- Table structure for table `Mesure`
 --
 
 CREATE TABLE `Mesure` (
-  `id_mes` mediumint NOT NULL,
-  `id_capt` tinyint NOT NULL,
+  `id_mes` mediumint(9) NOT NULL,
+  `id_capt` tinyint(4) NOT NULL,
   `date_mes` date NOT NULL,
-  `horarire_mes` time NOT NULL,
+  `horaire_mes` time NOT NULL,
   `valeur_mes` decimal(10,0) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Index pour les tables déchargées
+-- Dumping data for table `Mesure`
+--
+
+INSERT INTO `Mesure` (`id_mes`, `id_capt`, `date_mes`, `horaire_mes`, `valeur_mes`) VALUES
+(1, 1, '2023-06-02', '06:14:59', '18'),
+(2, 1, '2023-06-08', '06:14:59', '27'),
+(3, 2, '2023-06-08', '06:15:48', '19'),
+(4, 2, '2023-06-08', '06:15:48', '32'),
+(5, 3, '2023-06-08', '06:16:36', '192'),
+(6, 3, '2023-06-08', '06:16:36', '28'),
+(7, 4, '2023-05-08', '06:16:36', '11'),
+(8, 4, '2023-06-08', '06:16:36', '16');
+
+--
+-- Indexes for dumped tables
 --
 
 --
--- Index pour la table `Batiment`
+-- Indexes for table `Batiment`
 --
 ALTER TABLE `Batiment`
   ADD PRIMARY KEY (`id_bat`);
 
 --
--- Index pour la table `Capteur`
+-- Indexes for table `Capteur`
 --
 ALTER TABLE `Capteur`
   ADD PRIMARY KEY (`id_capt`),
   ADD KEY `id_bat` (`id_bat`);
 
 --
--- Index pour la table `Mesure`
+-- Indexes for table `Mesure`
 --
 ALTER TABLE `Mesure`
   ADD PRIMARY KEY (`id_mes`),
   ADD KEY `id_capt` (`id_capt`);
 
 --
--- AUTO_INCREMENT pour les tables déchargées
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT pour la table `Batiment`
+-- AUTO_INCREMENT for table `Batiment`
 --
 ALTER TABLE `Batiment`
-  MODIFY `id_bat` tinyint NOT NULL AUTO_INCREMENT;
+  MODIFY `id_bat` tinyint(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT pour la table `Capteur`
+-- AUTO_INCREMENT for table `Capteur`
 --
 ALTER TABLE `Capteur`
-  MODIFY `id_capt` tinyint NOT NULL AUTO_INCREMENT;
+  MODIFY `id_capt` tinyint(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT pour la table `Mesure`
+-- AUTO_INCREMENT for table `Mesure`
 --
 ALTER TABLE `Mesure`
-  MODIFY `id_mes` mediumint NOT NULL AUTO_INCREMENT;
+  MODIFY `id_mes` mediumint(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
