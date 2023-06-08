b4369d575b0826dca78751fe04d2099ddac162ef
Elie <133014379+CrapaudCrapaud@users.noreply.github.com>
2023-06-08 15:06:10 +0000

    réorganisation de l'architecture
    tous les dossiers et fichiers relatifs au site web sont désormais mis dans le dossier "site" pour plus de clarté

--- inc/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// $db = mysqli_connect("localhost", "john", "john123", "sae23") or die("Connexion à la base de données impossible.");
-$db = mysqli_connect("localhost", "root", "", "sae23") or die("Connexion à la base de données impossible.");
-
-mysqli_query($db, "SET NAMES 'utf8'");
-
-?>
